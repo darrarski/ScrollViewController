a4a7b3dad376e2d802903f9aad26494a7de6cbba
Dariusz Rybicki <darrarski@gmail.com>
2017-11-07 19:03:35 +0100

    Implement demo app

---
+++ DemoApp/View.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+class View: UIView {
+
+    init() {
+        super.init(frame: .zero)
+        addSubviews()
+        configureSubviews()
+        setupLayout()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        return nil
+    }
+
+    // MARK: Subviews
+
+    let textView = UITextView(frame: .zero)
+
+    private func addSubviews() {
+        addSubview(textView)
+    }
+
+    private func configureSubviews() {
+        textView.isScrollEnabled = false
+        textView.font = UIFont.preferredFont(forTextStyle: .body)
+        textView.layer.borderColor = UIColor.darkGray.cgColor
+        textView.layer.borderWidth = 0.5
+        textView.text = """
+        Lorem ipsum dolor sit amet mauris magna, gravida sem. Quisque augue. Maecenas nisl risus, euismod mi. Aliquam erat eu lobortis dapibus, accumsan lorem. Sed congue, lacus sagittis odio adipiscing dolor vel molestie venenatis nulla.
+
+        Put your text here...
+        """
+    }
+
+    // MARK: Layout
+
+    private func setupLayout() {
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.topAnchor.constraint(equalTo: topAnchor, constant: 16).isActive = true
+        textView.leftAnchor.constraint(equalTo: leftAnchor, constant: 16).isActive = true
+        textView.rightAnchor.constraint(equalTo: rightAnchor, constant: -16).isActive = true
+        textView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16).isActive = true
+    }
+
+}
--- DemoApp/ViewController.swift
+++ DemoApp/ViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import ScrollViewController
 
 class ViewController: UIViewController {
 
@@ -14,7 +15,29 @@ class ViewController: UIViewController {
 
     override func loadView() {
         view = UIView(frame: .zero)
-        view.backgroundColor = .white
+        view.backgroundColor = UIColor(red:0.94, green:0.94, blue:0.96, alpha:1.00)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        embed(scrollViewController)
+        scrollViewController.scrollView.alwaysBounceVertical = true
+        scrollViewController.contentView = View()
+    }
+
+    // MARK: Private
+
+    private let scrollViewController = ScrollViewController()
+
+    private func embed(_ viewController: UIViewController) {
+        addChildViewController(viewController)
+        view.addSubview(viewController.view)
+        viewController.view.translatesAutoresizingMaskIntoConstraints = false
+        viewController.view.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        viewController.view.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        viewController.view.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        viewController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        viewController.didMove(toParentViewController: self)
     }
 
 }
