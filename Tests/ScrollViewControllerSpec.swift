cc662a02d5fae6720d3e8142c16048a82842d80a
Dariusz Rybicki <darrarski@gmail.com>
2017-11-07 02:30:10 +0100

    Replace ScrollViewControllerTests with ScrollViewControllerSpec

---
+++ Tests/ScrollViewControllerSpec.swift
@@ -0,0 +1,17 @@
+import Quick
+import Nimble
+@testable import ScrollViewController
+
+class ScrollViewControllerSpec: QuickSpec {
+
+    override func spec() {
+        describe("ScrollViewController") {
+            let implemented = false
+
+            it("should be implemented") {
+                expect(implemented).to(beTrue())
+            }
+        }
+    }
+
+}
--- Tests/ScrollViewControllerTests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-class ScrollViewControllerTests: XCTestCase {
-    
-    func testExample() {
-        XCTAssert(false)
-    }
-
-}
