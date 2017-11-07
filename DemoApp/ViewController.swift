63124d6b1fb901909c24dcc45af52e371519c536
Dariusz Rybicki <darrarski@gmail.com>
2017-11-07 02:18:59 +0100

    Add Xcode project with demo app target

---
+++ DemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        return nil
+    }
+
+    // MARK: View
+
+    override func loadView() {
+        view = UIView(frame: .zero)
+        view.backgroundColor = .white
+    }
+
+}
