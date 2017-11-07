b04ecbf4b098a0f5b02963746f7782e183909ae9
Dariusz Rybicki <darrarski@gmail.com>
2017-11-07 17:45:06 +0100

    Add ScrollViewKeyboardAvoiderSpy

---
+++ Tests/ScrollViewKeyboardAvoiderSpy.swift
@@ -0,0 +1,13 @@
+@testable import ScrollViewKeyboardAvoider
+
+class ScrollViewKeyboardAvoiderSpy: ScrollViewKeyboardAvoiding {
+
+    var handledKeyboardChanges = [(CGRect, TimeInterval, UIScrollView)]()
+
+    // MARK: ScrollViewKeyboardAvoiding
+
+    func handleKeyboardFrameChange(_ frame: CGRect, animationDuration: TimeInterval, for scrollView: UIScrollView) {
+        handledKeyboardChanges.append((frame, animationDuration, scrollView))
+    }
+
+}
