e16140e33652ea0e98bccf8cd6bff5f2f8f45420
Dariusz Rybicki <darrarski@gmail.com>
2018-03-15 19:08:56 +0100

    Add UITabBarController to DemoApp

--- DemoApp/AppDelegate.swift
+++ DemoApp/AppDelegate.swift
@@ -8,7 +8,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        let navigationController = UINavigationController(rootViewController: ViewController())
+        let tabBarController = UITabBarController(nibName: nil, bundle: nil)
+        tabBarController.viewControllers = [navigationController]
+        window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
         return true
     }
