23c8abdb1a8b77a4ef57db29ebf39a757ec06a17
Enes Karaosman <eneskaraosman53@gmail.com>
2021-04-23 08:00:52 +0300

    [MOD] Min version back to 12 :D

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "ScrollViewController",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v12)
     ],
     products: [
         .library(
